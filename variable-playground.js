cf5c7c4b60f0e9f06111470e29280739104c9625
Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>
2016-02-05 22:41:29 +0530

    aAdd variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,32 @@
+/**
+ * Created by home on 2/5/16.
+ */
+/*
+
+var person = {
+    name: 'Ajinkya',
+    age: 26
+};
+
+function updateObj(obj){
+    /!*obj = {
+        name: 'Ajinkya',
+        age: 27
+    };*!/
+    obj.age = 27;
+}
+
+updateObj(person);
+console.log(person);
+*/
+
+// Array Example
+var grades = [15, 37];
+
+function addGrade(arr){
+    //arr.push(45);
+    arr = [15, 30, 45, 60];
+}
+
+addGrade(grades);
+console.log(grades);
